f46540da280b60c63d7b10860339de912ed41384
Jagerziel <fromnytosg@gmail.com>
2024-04-10 11:24:36 -0400

    Fixed centering issue

--- src/components/navbar/NavbarScoreboard.jsx
+++ src/components/navbar/NavbarScoreboard.jsx
@@ -1,16 +1,13 @@
 // Import React
-import React from 'react'
+import React from "react";
 
 // Import SCSS
-import './Navbar.scss'
+import "./Navbar.scss";
 
-export default function NavbarScoreboard( { } ) {
-
-    return (
-        <div
-            className='navbarscoreboard-container'
-        >
-            Scoreboard
-        </div>
-    )
+export default function NavbarScoreboard({}) {
+  return (
+    <div className="navbarscoreboard-container">
+      <div style={{border: '2px solid green'}}>Scoreboard</div>
+    </div>
+  );
 }
